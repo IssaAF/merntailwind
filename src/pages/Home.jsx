1234d944dd682131fd767714a0ecbaf42f4b4b78
IssaAF <abdulfataiissa@gmail.com>
2023-10-19 19:53:54 +0100

    Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-         Home
+    <div >
+        <span className='text-2x/9'>Home text-2xl/9</span><span className='text-2xl/2'>Home2 text-2xl/4</span>
     </div>
   )
 }
